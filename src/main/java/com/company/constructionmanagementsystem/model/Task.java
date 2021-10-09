88251c69545b2e8babc2e1cf25238d417b52c40e
Eric Chen <ericchen201896@gmail.com>
2021-10-09 09:38:56 -0700

    added in Task model

---
+++ src/main/java/com/company/constructionmanagementsystem/model/Task.java
@@ -0,0 +1,110 @@
+package com.company.constructionmanagementsystem.model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Task {
+    private Integer id;
+    @NotNull(message = "You must supply a project Id")
+    private Integer projectId;
+    private Integer employeeId;
+    @NotEmpty(message = "You must supply a name")
+    private String name;
+    private LocalDate startState;
+    private LocalDate deadline;
+    private String description;
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartState() {
+        return startState;
+    }
+
+    public void setStartState(LocalDate startState) {
+        this.startState = startState;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(projectId, task.projectId) && Objects.equals(employeeId, task.employeeId) && Objects.equals(name, task.name) && Objects.equals(startState, task.startState) && Objects.equals(deadline, task.deadline) && Objects.equals(description, task.description) && Objects.equals(status, task.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, projectId, employeeId, name, startState, deadline, description, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", projectId=" + projectId +
+                ", employeeId=" + employeeId +
+                ", name='" + name + '\'' +
+                ", startState=" + startState +
+                ", deadline=" + deadline +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
