d7ccb2f98cc59b0c457c02178f0e715902985246
Daniel.Yu <adam741963@gmail.com>
2021-10-09 11:01:00 -0700

    runTest

--- src/main/java/com/company/constructionmanagementsystem/repository/ProjectRepository.java
+++ src/main/java/com/company/constructionmanagementsystem/repository/ProjectRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer>{
 
-    List<Project> findByDeadLine(Integer deadline);
+//    List<Project> findByDeadLine(Integer deadline);
 
 }
