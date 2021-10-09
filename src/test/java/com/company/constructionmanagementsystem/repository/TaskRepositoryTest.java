70a9144260781b1d29e066bc8c1278d7272b207c
Daniel.Yu <adam741963@gmail.com>
2021-10-09 15:01:16 -0700

    TaskRepositoryTest

--- src/test/java/com/company/constructionmanagementsystem/repository/TaskRepositoryTest.java
+++ src/test/java/com/company/constructionmanagementsystem/repository/TaskRepositoryTest.java
@@ -1,10 +1,19 @@
 package com.company.constructionmanagementsystem.repository;
 
+import com.company.constructionmanagementsystem.model.Task;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDate;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class TaskRepositoryTest {
 
     @Autowired
@@ -25,5 +34,82 @@ public class TaskRepositoryTest {
         projectRepository.deleteAll();
 
         taskRepository.deleteAll();
+    }
+
+    @Test
+    public void addFindDeleteTask() {
+        LocalDate startDate = LocalDate.now();
+        LocalDate deadline = LocalDate.now();
+
+        Task task = new Task();
+        task.setName("Task One");
+        task.setStartDate(startDate);
+        task.setDeadline(deadline);
+        task.setDescription("This is a task.");
+        task.setStatus("In progress");
+
+        // add
+        task = taskRepository.save(task);
+
+        assertEquals(1, taskRepository.findAll().size());
+
+        // find
+        Task task1 = taskRepository.findById(task.getId()).get();
+
+        assertEquals(task1, task);
+
+        // delete
+        taskRepository.deleteById(task.getId());
+
+        assertEquals(0, taskRepository.findAll().size());
+    }
+
+    @Test
+    public void findAllTasks() {
+        LocalDate startDate = LocalDate.now();
+        LocalDate deadline = LocalDate.now();
+
+        Task task1 = new Task();
+        task1.setName("Task One");
+        task1.setStartDate(startDate);
+        task1.setDeadline(deadline);
+        task1.setDescription("This is task1.");
+        task1.setStatus("In progress");
+
+        taskRepository.save(task1);
+
+        Task task2 = new Task();
+        task2.setName("Task One");
+        task2.setStartDate(startDate);
+        task2.setDeadline(deadline);
+        task2.setDescription("This is task2.");
+        task2.setStatus("In progress");
+
+        taskRepository.save(task2);
+
+        assertEquals(2, taskRepository.findAll().size());
+
+    }
+
+    @Test
+    public void saveAndFlushTask() {
+        LocalDate startDate = LocalDate.now();
+        LocalDate deadline = LocalDate.now();
+
+        Task task = new Task();
+        task.setName("Task One");
+        task.setStartDate(startDate);
+        task.setDeadline(deadline);
+        task.setDescription("This is a task.");
+        task.setStatus("In progress");
+        task = taskRepository.save(task);
+
+        task.setStatus("Finish");
+
+        taskRepository.saveAndFlush(task);
+
+        Task task1 = taskRepository.findById(task.getId()).get();
+
+        assertEquals(task1, task);
     }
 }
