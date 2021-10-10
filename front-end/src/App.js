2562332fc0864c78b0718ff183c47f704247ccd7
Erik De Luna <codingerik1.gmail.com>
2021-10-10 15:03:20 -0500

    adding navbar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar/>
     </div>
   );
 }
