89e868bf23a8cb25b8984d1cd4ef1f8808f40773
Erik De Luna <codingerik1.gmail.com>
2021-10-12 16:18:37 -0500

    material micro service

---
+++ back-end/machine-warehouse-service/src/main/java/com/example/model/Machinery.java
@@ -0,0 +1,9 @@
+package com.example.model;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class Machine {
+}
---
+++ back-end/material-warehouse-service/src/main/java/com/company/repository/MaterialRepository.java
@@ -0,0 +1,13 @@
+package com.company.repository;
+
+import com.company.model.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialRepository extends JpaRepository<Material, Integer> {
+}
+
+
+
+
