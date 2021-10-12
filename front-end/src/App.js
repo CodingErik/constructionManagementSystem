720ff08ed1b15948bbe06890b6ed6c118fca1399
Daniel.Yu <adam741963@gmail.com>
2021-10-12 11:40:01 -0700

    login reducer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,11 @@
+import React, { useEffect } from 'react';
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,32 +14,69 @@ import MyProfile from './pages/MyProfile';
 import Projects from './pages/Projects';
 import Tasks from './pages/Tasks';
 import NoMatch from './pages/NoMatch';
-import SingleProjectPage from "./pages/SingleProjectPage";
+import SingleProjectPage from './pages/SingleProjectPage';
 import SingleTaskPage from './pages/SingleTaskPage';
+import { reducer, initialState } from './reducer/userInfoReducer';
+
+export const AuthContext = React.createContext();
 
 function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <div>
-          <Route path={["/MyProfile", "/Projects", "/Tasks", "/SingleProjectPage", "/SingleTaskPage", "/home"]}>
-            <NavBar />
-          </Route>
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const history = useHistory();
 
-          <Switch>
-            <Route exact path={['/', '/home']} component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={RegisterPage} />
-            <Route exact path='/MyProfile/:employeeID' component={MyProfile} />
-            <Route exact path='/Projects' component={Projects} />
-            <Route exact path='/Tasks' component={Tasks} />
-            <Route exact path='/SingleProjectPage/:projectId' component={SingleProjectPage} />
-            <Route exact path='/SingleTaskPage/:taskId' component={SingleTaskPage} />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+  useEffect(() => {}, [state]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      <div className='App'>
+        <Router>
+          <div>
+            <Route
+              path={[
+                '/MyProfile',
+                '/Projects',
+                '/Tasks',
+                '/SingleProjectPage',
+                '/SingleTaskPage',
+                '/home',
+                '/',
+              ]}
+            >
+              <NavBar />
+            </Route>
+
+            <Switch>
+              <Route exact path={['/', '/home']} component={Home} />
+              <Route exact path='/register' component={RegisterPage} />
+              <Route exact path='/login' component={Login} />
+              <Route
+                exact
+                path='/MyProfile/:employeeID'
+                component={MyProfile}
+              />
+              <Route exact path='/Projects' component={Projects} />
+              <Route exact path='/Tasks' component={Tasks} />
+              <Route
+                exact
+                path='/SingleProjectPage/:projectId'
+                component={SingleProjectPage}
+              />
+              <Route
+                exact
+                path='/SingleTaskPage/:taskId'
+                component={SingleTaskPage}
+              />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
