246febd574215f2acd9bbed3823cda8f6b8ce0c6
Erik De Luna <codingerik1.gmail.com>
2021-10-12 02:16:56 -0500

    adding warehouse and application properties

---
+++ back-end/material-warehouse-service/src/main/java/com/company/MaterialWarehouseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialWarehouseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialWarehouseServiceApplication.class, args);
+	}
+
+}
