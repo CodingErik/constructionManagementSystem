8bb1a8164b4f752ee8755418029b4ea2cc563cee
Eric Chen <ericchen201896@gmail.com>
2021-10-13 09:01:19 -0700

    added in Redirect To Login if token is null and cleaning up table code

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,20 +1,13 @@
 import React, { useEffect, useState } from "react";
-import OnGoingProjectsDisplay from "../components/home/OnGoingProjectsDisplay";
+import BriefProjectsDisplay from "../components/home/BriefProjectsDisplay";
+import BriefTasksDisplay from "../components/home/BriefTasksDisplay";
 import ProjectPieChart from "../components/home/ProjectPieChart";
 import EmployeeListTable from "../components/home/EmployeeListTable";
-import { ProjectAPI, EmployeeAPI } from "../api/index";
-
-let columnBooleans = {
-  projectId: false,
-  name: false,
-  status: false,
-  startDate: false,
-  deadline: false,
-};
-
+import { ProjectAPI, EmployeeAPI, TaskAPI } from "../api/index";
+import redirectIfTokenNull from "../components/RedirectHelper";
 
 function Home() {
-
+  redirectIfTokenNull();
   const [projectList, setProjectList] = useState([]);
   const [statusCount, setStatusCount] = useState({
     inProgress: 0,
@@ -22,79 +15,8 @@ function Home() {
     completed: 0,
   });
   const [employeeList, setEmployeeList] = useState([]);
+  const [taskList, setTaskList] = useState([]);
 
-  // Filter column descending and ascending
-  const handleProjectColumnHeaderClick = (target) => {
-    const sort_by = (field, reverse, primer) => {
-      const key = primer
-        ? function (x) {
-            return primer(x[field]);
-          }
-        : function (x) {
-            return x[field];
-          };
-      reverse = !reverse ? 1 : -1;
-
-      return function (a, b) {
-        return (a = key(a)), (b = key(b)), reverse * ((a > b) - (b > a));
-      };
-    };
-
-    if (target === "Name") {
-      if (columnBooleans.name) {
-        columnBooleans.name = false;
-      } else {
-        columnBooleans.name = true;
-      }
-      setProjectList(
-        [...projectList].sort(
-          sort_by("name", columnBooleans.name, (a) => a.toUpperCase())
-        )
-      );
-    } else if (target === "ProjectId") {
-      if (columnBooleans.projectId) {
-        columnBooleans.projectId = false;
-      } else {
-        columnBooleans.projectId = true;
-      }
-      setProjectList(
-        [...projectList].sort(sort_by("id", columnBooleans.projectId, parseInt))
-      );
-    } else if (target === "Status") {
-      if (columnBooleans.status) {
-        columnBooleans.status = false;
-      } else {
-        columnBooleans.status = true;
-      }
-      setProjectList(
-        [...projectList].sort(
-          sort_by("status", columnBooleans.status, (a) => a.toUpperCase())
-        )
-      );
-    } else if (target === "StartDate") {
-      if (columnBooleans.startDate) {
-        columnBooleans.startDate = false;
-      } else {
-        columnBooleans.startDate = true;
-      }
-      setProjectList(
-        [...projectList].sort(
-          sort_by("startDate", columnBooleans.startDate, (a) => a.toUpperCase())
-        )
-      );
-    } else if (target === "Deadline") {
-      if (columnBooleans.deadline) {
-        columnBooleans.deadline = false;
-      } else {
-        columnBooleans.deadline = true;
-      }
-      setProjectList(
-        [...projectList].sort(
-          sort_by("deadline", columnBooleans.deadline, (a) => a.toUpperCase())
-        )
-      );
-    }
-  };
 
   useEffect(() => {
     ProjectAPI.getAllProjects().then((response) => {
@@ -118,9 +40,13 @@ function Home() {
         }
       });
     });
+
     EmployeeAPI.getAllEmployees().then((response) => {
       setEmployeeList(response.data);
-      console.log(response.data);
+    });
+
+    TaskAPI.getAllTasks().then((response) => {
+      setTaskList(response.data);
     });
   }, []);
 
@@ -128,10 +54,12 @@ function Home() {
     <div className="container">
       <div className="row">
         <div className="col col-lg-7">
-          <OnGoingProjectsDisplay
-            projectList={projectList}
-            handleProjectColumnHeaderClick={handleProjectColumnHeaderClick}
-          ></OnGoingProjectsDisplay>
+          <div className="row">
+            <BriefProjectsDisplay
+              originalProjectLists={projectList}
+            ></BriefProjectsDisplay>
+          </div>
+          <div className="row"></div>
         </div>
         <div className="col col-lg-5">
           <div className="row">
