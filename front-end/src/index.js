8db3d78b04b0abd6741e12ed7f7ce8bc956d5b01
Eric Chen <ericchen201896@gmail.com>
2021-10-13 07:17:20 -0700

    added token saved and using token on each page to check authorization

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap.min.css';
+import axios from "axios";
+
+
+axios.interceptors.request.use(request => {
+  // add authorization headers for requests to own api (request url starts with
+  // "/api" exactly)
+  const token = JSON.parse(localStorage.getItem("token"));
+  if (token && request.url.includes(`/api`)) {
+    request.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return request;
+});
 
 ReactDOM.render(
   <React.StrictMode>
