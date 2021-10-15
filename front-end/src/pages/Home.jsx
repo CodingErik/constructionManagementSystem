27361581cb6ef402af146bb7c0bcefadfffe1946
Eric Chen <ericchen201896@gmail.com>
2021-10-15 16:43:59 -0700

    added in warehouse icons

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import BriefProjectsDisplay from '../components/home/BriefProjectsDisplay';
-import BriefTasksDisplay from '../components/home/BriefTasksDisplay';
-import ProjectPieChart from '../components/home/ProjectPieChart';
-import EmployeeListTable from '../components/home/EmployeeListTable';
-import Materials from '../components/materials/Materials';
-import Machinery from '../components/machinery/Machinery';
+import React, { useEffect, useState } from "react";
+import BriefProjectsDisplay from "../components/home/BriefProjectsDisplay";
+import BriefTasksDisplay from "../components/home/BriefTasksDisplay";
+import ProjectPieChart from "../components/home/ProjectPieChart";
+import EmployeeListTable from "../components/home/EmployeeListTable";
+import Resources from "../components/resources/Resources";
+import steelIcon from "../assets/steel.png";
+import brickIcon from "../assets/brick.png";
+import lumberIcon from "../assets/lumber.png";
+import cementIcon from "../assets/cement.png";
+import craneIcon from "../assets/crane.png";
+import forkliftIcon from "../assets/forklift.png";
+import ladderIcon from "../assets/ladder.png";
+import drillIcon from "../assets/drill.png";
+
 import {
   ProjectAPI,
   EmployeeAPI,
   TaskAPI,
   MaterialAPI,
   MachineryAPI,
-} from '../api/index';
-import redirectIfTokenNull from '../components/RedirectHelper';
+} from "../api/index";
+import redirectIfTokenNull from "../components/RedirectHelper";
 
 function Home() {
   redirectIfTokenNull();
@@ -31,17 +39,17 @@ function Home() {
     ProjectAPI.getAllProjects().then((response) => {
       setProjectList([...response.data]);
       response.data.forEach((project) => {
-        if (project.status === 'in_progress') {
+        if (project.status === "in_progress") {
           setStatusCount((prevState) => ({
             ...prevState,
             inProgress: prevState.inProgress++,
           }));
-        } else if (project.status === 'completed') {
+        } else if (project.status === "completed") {
           setStatusCount((prevState) => ({
             ...prevState,
             completed: prevState.completed++,
           }));
-        } else if (project.status === 'cancelled') {
+        } else if (project.status === "cancelled") {
           setStatusCount((prevState) => ({
             ...prevState,
             cancelled: prevState.cancelled++,
@@ -62,53 +70,98 @@ function Home() {
       });
 
     MaterialAPI.getAllMaterialsInventory().then((response) => {
-      setMaterials(response.data);
+      const materialsHolder = {
+        steel: {
+          name: "Steel",
+          amount: response.data?.steel,
+          icon: steelIcon,
+        },
+        brick: {
+          name: "Brick",
+          amount: response.data?.brick,
+          icon: brickIcon,
+        },
+        lumber: {
+          name: "Lumber",
+          amount: response.data?.lumber,
+          icon: lumberIcon,
+        },
+        cement: {
+          name: "Cement",
+          amount: response.data?.cement,
+          icon: cementIcon,
+        },
+      };
+      setMaterials(materialsHolder);
     });
 
     MachineryAPI.getAllMachineryInventory().then((response) => {
-      setMachinery(response.data);
+      const machineHolder = {
+        crane: {
+          name: "Crane",
+          amount: response.data?.crane,
+          icon: craneIcon,
+        },
+        forklift: {
+          name: "Forklift",
+          amount: response.data?.forklift,
+          icon: forkliftIcon,
+        },
+        ladder: {
+          name: "Ladder",
+          amount: response.data?.ladder,
+          icon: ladderIcon,
+        },
+        drill: {
+          name: "Drill",
+          amount: response.data?.drill,
+          icon: drillIcon,
+        }
+      };
+      console.log(machineHolder);
+      setMachinery(machineHolder);
     });
   }, []);
 
   return (
     <div
-      className='container mt-3 home'
+      className="container mt-3 home"
       style={{
-        overflowX: 'hidden',
+        overflowX: "hidden",
       }}
     >
-      <div className='row mb-3 mt-3'>
-        <div className='col col-xl-6 ml-1 mr-1'>
+      <div className="row mb-3 mt-3">
+        <div className="col col-xl-6 ml-1 mr-1">
           <BriefProjectsDisplay
             originalProjectLists={projectList}
           ></BriefProjectsDisplay>
         </div>
-        <div className='col col-xl-6 ml-1 mr-1'>
+        <div className="col col-xl-6 ml-1 mr-1">
           <ProjectPieChart statusCount={statusCount}></ProjectPieChart>
         </div>
       </div>
 
-      <div className='row mb-3 mt-3'>
-        <div className='col col-xl-6 ml-1 mr-1'>
-          <div className='row mb-3'>
-            <div className='row mt-3' style={{ minHeight: '445px' }}>
-              <div className='col col-xl-6'>
-                <Materials materials={materials} />
+      <div className="row mb-3 mt-3">
+        <div className="col col-xl-6 ml-1 mr-1">
+          <div className="row mb-3">
+            <div className="row mt-3" style={{ minHeight: "445px" }}>
+              <div className="col col-xl-6">
+                <Resources resources={materials} title="Materials" denominator="/1000 lbs" />
               </div>
-              <div className='col col-xl-6'>
-                <Machinery machinery={machinery} />
+              <div className="col col-xl-6">
+                <Resources resources={machinery} title="Machines" denominator="/30 units"/>
               </div>
             </div>
           </div>
 
-          <div className='row mt-4'>
+          <div className="row mt-4">
             <BriefTasksDisplay
               originalTaskList={taskList}
               projectIsNotANumber={true}
             ></BriefTasksDisplay>
           </div>
         </div>
-        <div className='col col-xl-6 ml-1 mr-1'>
+        <div className="col col-xl-6 ml-1 mr-1">
           <EmployeeListTable employeeList={employeeList} />
         </div>
       </div>
