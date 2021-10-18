8558b41ae6181aa470e1a5e43eeb51ef3237c6d1
Amal Janabayev <amalj2426@gmail.com>
2021-10-17 18:31:59 -0700

    Added readme

--- back-end/machine-warehouse-service/src/test/java/com/example/repository/MachineryRepositoryTest.java
+++ back-end/machine-warehouse-service/src/test/java/com/example/repository/MachineryRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.example.repositoty;
+package com.example.repository;
 
 import com.example.model.Machinery;
 import org.junit.Before;
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.Transient;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
