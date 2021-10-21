4c182d9633c1692ea58072d6a2158d0c57356d40
Eric Chen <ericchen201896@gmail.com>
2021-10-21 09:04:18 -0700

    got expo navigation working

---
+++ front-end-mobile/pages/MyProfile.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+
+function MyProfile() {
+    return (
+        <View>
+            <Text>My Profile</Text>
+        </View>
+    )
+}
+
+export default MyProfile
